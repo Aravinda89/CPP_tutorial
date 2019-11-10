2ad4d195ddc23473d626f55035f98ad0ebb9ac0b
aravindagayan <1513223865@qq.com>
2019-11-10 20:58:47 +0800

    Initial Commit

---
+++ CPP_tutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPP_tutorial
+//
+//  Created by Aravinda Gayan on 11/10/19.
+//  Copyright © 2019 Aravinda Gayan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
