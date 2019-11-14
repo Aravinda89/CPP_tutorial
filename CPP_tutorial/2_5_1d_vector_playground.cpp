7f53b81f28a223db1a929b6e6a2dc948ae4a76ea
aravindagayan <1513223865@qq.com>
2019-11-14 23:57:52 +0800

    1d vector update

--- CPP_tutorial/2_5_1d_vector_playground.cpp
+++ CPP_tutorial/2_5_1d_vector_playground.cpp
@@ -15,6 +15,7 @@ using namespace std;
 // inside the main function. 
 
 vector<float> substract_vectors(vector<float> vector_1, vector<float> vector_2);
+vector<float> multiply_vectors(vector<float> vector_1, vector<float> vector_2);
 
 int main(){
 	vector<float> vector_1 (3);
@@ -30,10 +31,22 @@ int main(){
 	vector<float> sub1 (vector_1.size());
     sub1 = substract_vectors(vector_1, vector_2);
 
+    cout << "Substraction" << endl;
 	for (int i = 0; i < sub1.size(); ++i)
 	{
         cout<< sub1[i]<< endl;
 	}
+
+	vector<float> mul (vector_1.size());
+	mul = multiply_vectors(vector_1, vector_2);
+    
+    cout << "\n";
+    cout << "Multification" << endl;
+	for (int i = 0; i <vector_1.size(); ++i)
+	{
+		cout<< mul[i] << endl;
+	}
+
 
 	return 0;
 }
@@ -46,4 +59,15 @@ vector<float> substract_vectors(vector<float> vector_1, vector<float> vector_2){
 		substraction[i] = vector_1[i] - vector_2[i];
 	}
     return substraction;
+}
+
+vector<float> multiply_vectors(vector<float> vector_1, vector<float> vector_2){
+
+	vector<float> mutification (vector_1.size());
+
+	for (int i = 0; i < vector_1.size(); ++i)
+	{
+		mutification[i] = vector_1[i] * vector_2[i];
+	}
+	return mutification;
 }
