b1c52cdf3ddf0e78855b517cf2df7a7ad4708828
aravindagayan <1513223865@qq.com>
2019-11-20 23:21:53 +0800

    cin >>

---
+++ CPP_tutorial/3_3_cin.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int num_1;
+	int num_2;
+
+	cout << "Enter a number between 1~100" << endl;
+	cin >> num_1;
+
+	cout << "Enter a number between 1~100" << endl;
+	cin >> num_2;
+
+	cout << "Difference" << endl;
+	cout << num_1 - num_2 << endl;
+
+	return 0;
+}
