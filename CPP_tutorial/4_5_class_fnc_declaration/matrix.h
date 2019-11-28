29af55eda2c74de19f3ef280931ecd41d091e300
aravindagayan <1513223865@qq.com>
2019-11-28 23:44:37 +0800

    Update

--- CPP_tutorial/4_5_class_fnc_declaration/matrix.h
+++ CPP_tutorial/4_5_class_fnc_declaration/matrix.h
@@ -17,6 +17,8 @@ class Matrix
         ** 1. An empty constructor function
         ** 2. A constructor function that accepts a 2-dimensional vector
         */
+        Matrix();
+        Matrix(std::vector< std::vector <float> > );
         
         /*
         ** TODO: Declare set and get functions for the three private variables.
@@ -27,8 +29,13 @@ class Matrix
         ** The setGrid does not return anything and has a 2-D vector input
         ** getGrid returns a 2-D vector and has no input
         ** getRows returns a size_type and has no input
-        ** get Cols returns a size_type and has no input
+        ** getCols returns a size_type and has no input
         */
+        void setGrid(std::vector< std::vector<float> >);
+
+        std::vector< std::vector<float> getGrid();
+        std::vector::size_type getRows();
+        std::vector::size_type getCols();
         
         /*
         ** TODO: Declare the matrix functions. In a following exercise, you
@@ -39,5 +46,8 @@ class Matrix
         ** matrix_addition receives a Matrix and outputs a 2D vector
         ** matrix_print has no inputs and no outputs
         */
+        std::vector< std::vector<float> >matrix_transpose();
+        std::vector< std::vector<float> >matrix_addition(Matrix);
+        void matrix_print();
 };
         
