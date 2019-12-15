35829058327e425374610ea167586ce1b9ebfa6f
aravindagayan <1513223865@qq.com>
2019-12-15 20:32:31 +0800

    Dead code

--- CPP_tutorial/7_Optimization_Practice/home/DeadCode/matrix_addition_improved.cpp
+++ CPP_tutorial/7_Optimization_Practice/home/DeadCode/matrix_addition_improved.cpp
@@ -11,14 +11,12 @@ vector < vector <int> > matrix_addition_improved (vector < vector <int> > matrix
     vector<int>::size_type cols_a = matrixa[0].size();
     vector<int>::size_type cols_b = matrixb[0].size();
 
-    // default zero vector
-    vector <vector <int> > default_vector(rows_a,vector<int>(cols_b));
-
     // if both matrices have the same size, calculate and return the sum
     // otherwise check if the number of rows and columns are not equal and return a matrix of zero
+
+    vector < vector <int> > matrix_sum(matrixa.size(), vector<int>(matrixa[0].size()));
+
     if (rows_a == rows_b && cols_a == cols_b) {
-    
-        vector < vector <int> > matrix_sum(matrixa.size(), vector<int>(matrixa[0].size()));
         
         for (unsigned int i = 0; i < matrixa.size(); i++) {
             for (unsigned int j = 0; j < matrixa[0].size(); j++) {
@@ -27,14 +25,6 @@ vector < vector <int> > matrix_addition_improved (vector < vector <int> > matrix
         }
         return matrix_sum;
     }
-    else if (rows_a != rows_b) {
-        return default_vector;
-    }
-    else if (cols_a != cols_b) {
-        return default_vector;
-    }
-    else {
-        return default_vector;
-    }
     
+    return matrix_sum;    
 }
