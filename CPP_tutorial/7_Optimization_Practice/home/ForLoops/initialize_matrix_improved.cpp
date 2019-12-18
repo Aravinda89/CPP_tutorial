e019b9170615119f482c65b40afac2460c4aa81b
aravindagayan <1513223865@qq.com>
2019-12-18 23:37:35 +0800

    for loop optimization

--- CPP_tutorial/7_Optimization_Practice/home/ForLoops/initialize_matrix_improved.cpp
+++ CPP_tutorial/7_Optimization_Practice/home/ForLoops/initialize_matrix_improved.cpp
@@ -3,16 +3,8 @@
 using namespace std;
 vector < vector<int> > initialize_matrix_improved(int num_rows, int num_cols, int initial_value) {
     
-    vector < vector<int> > matrix;
-    vector<int> new_row;
-    
-    for (int i = 0; i < num_rows; i++) {
-        new_row.clear();
-        for (int j = 0; j < num_cols; j++) {
-            new_row.push_back(initial_value);
-        }
-        matrix.push_back(new_row);
-    }    
+    vector <int> new_row(num_cols, initial_value);
+    vector < vector<int> > matrix(num_rows, new_row);
     
     return matrix;
 }
