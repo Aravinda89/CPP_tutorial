31e414968506e881abb332b4f51810a3150df6c3
aravindagayan <1513223865@qq.com>
2019-12-26 21:57:24 +0800

    reserve

--- CPP_tutorial/7_Optimization_Practice/home/VectorStorage/reserved.cpp
+++ CPP_tutorial/7_Optimization_Practice/home/VectorStorage/reserved.cpp
@@ -6,7 +6,9 @@ vector< vector<int> > reserved(int rows, int cols, int initial_value) {
     
     // OPTIMIZE: use the reserve method with the matrix and new_row variables
     vector< vector<int> > matrix;
+    matrix.reserve(rows);
     vector<int> new_row;
+    new_row.reserve(cols);
     
     for (int i = 0; i < rows; i++) {
         new_row.clear();
